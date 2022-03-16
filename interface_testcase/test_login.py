8d70fd62b09b58a9eb44af765d2e5bf9f219acd0
zhaotong <18210192027@163.com>
2022-03-17 00:01:43 +0800

    fixture-zhaotong-202203162355

--- interface_testcase/test_login.py
+++ interface_testcase/test_login.py
@@ -1,27 +1,33 @@
 import pytest
 
-def test_04_func():
-    print("测试函数")
+
+@pytest.fixture(scope="function", params=['张三', '李四'], name='aaa')
+# @pytest.fixture(scope="class", autouse=True)
+# @pytest.fixture(scope="module  ", autouse=True)
+def my_fixture(request):
+    print('前置方法，可实现全部或部分用例的后置')
+    yield request.param  # return和yield都执行返回，yield后面可跟代码，return则不可
+    print("后置方法，可实现全部或部分用例的后置")
+    # return request.param  # 固定写法，fixture参数名带复数，request 属性名不带复数
 
 class TestLogin:
-    usernum = 1
-    @pytest.mark.run(order=4)
-    @pytest.mark.smoke
+
+    # def setup_class(self):
+    #     print('\n每个类执行前的初始化，比如：创建日志，创建数据库，创建接口的请求对象\n')
+    #
+    # def setup(self):
+    #     print('\n在执行测试用例之前的初始化')
+
     def test_01_login(self):
-        print('测试登录')
+         print('测试登录')
 
-    @pytest.mark.run(order=1)
-    @pytest.mark.usermanage
-    def test_05_register(self):
+    # def test_05_register(self, my_fixture):
+    def test_05_register(self, aaa):
         print('测试注册')
-        # assert 1==2
+        print('\n用户'+str(aaa)+'注册成功')
 
-    @pytest.mark.run(order=2)
-    @pytest.mark.skip(usernum > 1, reason = "不可超过两人同时在线")
-    def test_06_rename(self):
-        print('测试重命名')
-
-    @pytest.mark.run(order=3)
-    @pytest.mark.smoke
-    def test_07_change(self):
-        print('测试修改')
+    # def teardown(self):
+    #     print('\n在执行测试用例之后扫尾工作\n')
+    #
+    # def teardown_class(self):
+    #     print('\n每个类执行结束的扫尾工作，比如：销毁对象\n')
