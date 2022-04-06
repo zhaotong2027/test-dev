14ba68472ebd64c42232a19cf10dc0b39aaf8329
zhaotong2027 <18210192027@163.com>
2022-04-06 23:59:26 +0800

    report-zhaotong-202204062359

--- all.py
+++ all.py
@@ -1,10 +1,11 @@
 import os
 import pytest
 
+# noinspection PyPackageRequirements
 if __name__ == '__main__':
     # pytest.main(['-vs', '../interface_testcase', '--alluredir', 'result'])
     # os.system('allure generate ./result -o ./report --clean')
-    pytest.main(['-vs', '../interface_testcase', '--alluredir', 'temp'])
+    pytest.main(['-vs', './interface_testcase', '--alluredir', 'temp'])
     os.system('allure generate ./temp -o ./report --clean')
     # pytest.main(['-vs', '../interface_testcase', '--reruns=2'])
     # pytest.main(['-vs', '../interface_testcase', '-n 2'])
